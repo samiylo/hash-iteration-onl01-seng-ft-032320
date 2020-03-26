00d1c5e470e16d1ffbac4bedd2ef950ee3c97a74
Samiylo Kryshu <skryshu@gmail.com>
2020-03-26 20:04:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,6 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(bkids)
-  birthday_kids.each do |kids_name, age|
+  bkids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   
